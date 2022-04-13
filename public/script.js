f86b9071855d4916ee3b09433186d8ad9f2055a4
cbrock22 <70349537+cbrock22@users.noreply.github.com>
2022-04-13 13:34:28 -0600

    updating client's ability to save chat
    the client can save chat BEFORE they disconnect from the server

--- public/script.js
+++ public/script.js
@@ -10,6 +10,11 @@ let connectionSpan;
 let connectButton;
 let createChat;
 let connChat;
+let username;
+var user = null;
+var chatLog = new Array;
+var userLog = new Array;
+
 
 function setup() {
   // get all the DOM elements that need listeners:
@@ -19,13 +24,39 @@ function setup() {
   outgoingText = document.getElementById('outgoing');
   connectionSpan = document.getElementById('connection');
   connectButton = document.getElementById('connectButton');
+  username = document.getElementById('username');
+  save = document.getElementById('saveButton');
   // set the listeners:
   
   outgoingText.addEventListener('change', sendMessage);
   connChat.addEventListener('change', connectToChat);
+  username.addEventListener('change', checkUsername);
   connectButton.addEventListener('click', changeConnection);
   createChat.addEventListener('click', createNewChat);
+  save.addEventListener('click', saveChat);
   openSocket(serverURL);
+}
+
+function saveChat(){
+  //var path = require('path');
+  console.log(chatLog);
+  var file_name = 'test.txt';
+  var file = new Blob(chatLog, {type: "text/plain;charset=utf-8"});
+    if (window.navigator.msSaveOrOpenBlob) 
+        window.navigator.msSaveOrOpenBlob(file, file_name);
+    else { 
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = file_name;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+  
 }
 
 function openSocket(url) {
@@ -36,6 +67,15 @@ function openSocket(url) {
   socket.addEventListener('message', readIncomingMessage);
 }
 
+function checkUsername(){
+  if(user == null){
+    user = username.value;
+  }
+  else{
+    alert("You cannot change your username once it is set for security purposes.");
+  }
+  username.value="";
+}
 
 function changeConnection(event) {
   // open the connection if it's closed, or close it if open:
@@ -71,8 +111,12 @@ function closeConnection() {
 function readIncomingMessage(event) {
   // display the incoming message:
   //incomingSpan.innerHTML = event.data;
+  chatLog.push(event.data + '\n');
+  var un = event.data;
+  un = un.split(':');
+  if(!userLog.includes(un)){userLog.push(un);}
   var tag = document.createElement("p"); 
-  var text = document.createTextNode("> " +event.data); 
+  var text = document.createTextNode("\t> " +event.data); 
   tag.appendChild(text); 
   var element = document.getElementsByTagName("body")[0];
   element.appendChild(tag); 
@@ -81,8 +125,16 @@ function readIncomingMessage(event) {
 function sendMessage() {
   //if the socket's open, send a message:
   if (socket.readyState === WebSocket.OPEN) {
-    socket.send(outgoingText.value);
+    socket.send(user + " : " + outgoingText.value);
+    chatLog.push(user + " : " + outgoingText.value + '\n');
   }
+  var tag = document.createElement("p"); 
+  var text = document.createTextNode(outgoingText.value + " \t< You\t\t");
+  tag.style.textAlign = "center"; 
+  tag.appendChild(text); 
+  var element = document.getElementsByTagName("body")[0];
+  element.appendChild(tag);
+  outgoingText.value = "";
 }
 
 // add a listener for the page to load:
